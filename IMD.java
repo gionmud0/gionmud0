3fe7679b845dc082de3cf5d4720a6e0145e49798
gionmud0 <135476335+gionmud0@users.noreply.github.com>
2024-02-24 19:20:05 +0400

    Update IMD.java

--- IMD.java
+++ IMD.java
@@ -1,13 +1,1 @@
-package netflix;
-
-import android.app.Activity;
-import android.os.Bundle;
-
-public class MainActivity extends Activity {
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-    
-}
+{}
